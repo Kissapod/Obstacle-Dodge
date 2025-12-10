e17b10023624d52b1c671a1feda363acae9791a9
Kissapod <akselkotic@gmail.com>
2025-12-10 22:22:36 +0300

    Added a new object Trigger Volume and a new script TriggerProjectile for activate Projectile objects

---
+++ Assets/Scripts/TriggerProjectile.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class TriggerProjectile : MonoBehaviour
+{
+    [SerializeField] GameObject projectile_1;
+    [SerializeField] GameObject projectile_2;
+    [SerializeField] GameObject projectile_3;
+    [SerializeField] GameObject projectile_4;
+    [SerializeField] GameObject projectile_5;
+    [SerializeField] GameObject projectile_6;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            projectile_1.SetActive(true);
+            projectile_2.SetActive(true);
+            projectile_3.SetActive(true);
+            projectile_4.SetActive(true);
+            projectile_5.SetActive(true);
+            projectile_6.SetActive(true);
+        }
+    }
+
+}
